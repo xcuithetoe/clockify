92b803058aa78425db129c6c9245e3ed50ec905c
xcuithetoe <78119824+xcuithetoe@users.noreply.github.com>
2023-02-23 09:06:19 -0800

    commit before changing branch to see how the vertical thing works

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,19 +1,26 @@
  // get current time
  var dateInfo = new Date();
- var hr = dateInfo.getHours() > 12 ? dateInfo.getHours() - 12 : dateInfo.getHours(),
+ let hr = dateInfo.getHours() > 12 ? dateInfo.getHours() - 12 : dateInfo.getHours(),
    min = dateInfo.getMinutes(),
    sec = dateInfo.getSeconds(),
    milsec = dateInfo.getMilliseconds();
 
 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+if (min < 10) {
+  min = "0" + min;
+}
+console.log(min);
 
+if (hr < 10) {
+  hr = "0" + hr;
+}
 let day = weekday[dateInfo.getDay()];
 var month = dateInfo.getMonth() + 1; //months from 1-12
 var date = dateInfo.getDate();
 
 newdate = day + ", " + month + "/" + date;
 
-document.getElementsByClassName("clock")[0].innerHTML = newdate;
+document.getElementsByClassName("clock")[0].innerHTML = newdate +"\n" + hr + ":" + min;
 
 
 /*
